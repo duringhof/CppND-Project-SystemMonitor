6d05ce3f965d823f375e134a6b993c3f926cba3d
Hans-Martin Duringhof <hmduringhof@gmail.com>
2020-12-23 13:06:10 +0100

    Implemented processor utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,9 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  long total = LinuxParser::Jiffies();
+  long active = LinuxParser::ActiveJiffies();
+  return active * (1.f / total);
+}
