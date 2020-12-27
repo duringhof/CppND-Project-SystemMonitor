d123e89558347c770097b43854acbeb7a43b90dc
Hans-Martin <48694653+duringhof@users.noreply.github.com>
2020-12-27 16:52:17 +0100

    Update processor.cpp
    Cleanup TODO comments

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,7 +1,7 @@
 #include "processor.h"
 #include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
+// Returning the aggregate CPU utilization
 float Processor::Utilization() {
   long total = LinuxParser::Jiffies();
   long active = LinuxParser::ActiveJiffies();
