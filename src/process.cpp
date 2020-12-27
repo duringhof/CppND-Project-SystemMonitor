66809868576fad2db06764426a4a1f016ad9600b
Hans-Martin <48694653+duringhof@users.noreply.github.com>
2020-12-27 16:51:20 +0100

    Update process.cpp
    Cleanup TODO comments

--- src/process.cpp
+++ src/process.cpp
@@ -17,10 +17,10 @@ Process::Process(int pid) {
   pid_ = pid;
 }
 
-// TODO: Return this process's ID
+// Returning this process's ID
 int Process::Pid() const { return pid_; }
 
-// TODO: Return this process's CPU utilization
+// Returning this process's CPU utilization
 float Process::CpuUtilization() const {
   float act_time = float(LinuxParser::ActiveJiffies(Pid()))/100.f;
   float up_time = float(LinuxParser::UpTime(Pid()));
@@ -28,17 +28,17 @@ float Process::CpuUtilization() const {
   return float(util);
 }
 
-// TODO: Return the command that generated this process
+// Returning the command that generated this process
 string Process::Command() { return LinuxParser::Command(Pid()); }
 
-// TODO: Return this process's memory utilization
+// Returning this process's memory utilization
 string Process::Ram() const { return LinuxParser::Ram(Pid()); }
 
-// TODO: Return the user (name) that generated this process
+// Returning the user (name) that generated this process
 string Process::User() { return LinuxParser::User(Pid()); }
 
-// TODO: Return the age of this process (in seconds)
+// Returning the age of this process (in seconds)
 long int Process::UpTime() { return LinuxParser::UpTime(Pid()); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
+// Overloading the "less than" comparison operator for Process objects
 bool Process::operator<(Process const& a) const { return stol(Ram()) < stol(a.Ram()); }
